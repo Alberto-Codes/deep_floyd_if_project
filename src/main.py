0c078453f285bf5f79d044aff62746366752489f
Alberto Nieto <alberto.nieto.80@gmail.com>
2023-04-30 11:05:12 -0700

    quickstart attempt

---
+++ src/main.py
@@ -0,0 +1,75 @@
+from huggingface_hub import login
+from dotenv import load_dotenv, find_dotenv
+import os
+load_dotenv(find_dotenv())
+
+login(os.getenv('HUGGINGFACE_API_KEY'))
+
+from diffusers import DiffusionPipeline
+from diffusers.utils import pt_to_pil
+import torch
+
+# stage 1
+stage_1 = DiffusionPipeline.from_pretrained("DeepFloyd/IF-I-XL-v1.0", variant="fp16", torch_dtype=torch.float16)
+stage_1.enable_xformers_memory_efficient_attention()  # remove line if torch.__version__ >= 2.0.0
+stage_1.enable_model_cpu_offload()
+
+# stage 2
+stage_2 = DiffusionPipeline.from_pretrained(
+    "DeepFloyd/IF-II-L-v1.0", text_encoder=None, variant="fp16", torch_dtype=torch.float16
+)
+stage_2.enable_xformers_memory_efficient_attention()  # remove line if torch.__version__ >= 2.0.0
+stage_2.enable_model_cpu_offload()
+
+# stage 3
+safety_modules = {"feature_extractor": stage_1.feature_extractor, "safety_checker": stage_1.safety_checker, "watermarker": stage_1.watermarker}
+stage_3 = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-x4-upscaler", **safety_modules, torch_dtype=torch.float16)
+stage_3.enable_xformers_memory_efficient_attention()  # remove line if torch.__version__ >= 2.0.0
+stage_3.enable_model_cpu_offload()
+
+prompt = 'a photo of a kangaroo wearing an orange hoodie and blue sunglasses standing in front of the eiffel tower holding a sign that says "very deep learning"'
+
+# text embeds
+prompt_embeds, negative_embeds = stage_1.encode_prompt(prompt)
+
+generator = torch.manual_seed(0)
+
+# stage 1
+image = stage_1(prompt_embeds=prompt_embeds, negative_prompt_embeds=negative_embeds, generator=generator, output_type="pt").images
+pt_to_pil(image)[0].save("./if_stage_I.png")
+
+# stage 2
+image = stage_2(
+    image=image, prompt_embeds=prompt_embeds, negative_prompt_embeds=negative_embeds, generator=generator, output_type="pt"
+).images
+pt_to_pil(image)[0].save("./if_stage_II.png")
+
+# stage 3
+image = stage_3(prompt=prompt, image=image, generator=generator, noise_level=100).images
+image[0].save("./if_stage_III.png")
+
+from deepfloyd_if.pipelines import dream
+
+prompt = 'ultra close-up color photo portrait of rainbow owl with deer horns in the woods'
+count = 4
+
+result = dream(
+    t5=t5, if_I=if_I, if_II=if_II, if_III=if_III,
+    prompt=[prompt]*count,
+    seed=42,
+    if_I_kwargs={
+        "guidance_scale": 7.0,
+        "sample_timestep_respacing": "smart100",
+    },
+    if_II_kwargs={
+        "guidance_scale": 4.0,
+        "sample_timestep_respacing": "smart50",
+    },
+    if_III_kwargs={
+        "guidance_scale": 9.0,
+        "noise_level": 20,
+        "sample_timestep_respacing": "75",
+    },
+)
+
+if_III.show(result['III'], size=14)
